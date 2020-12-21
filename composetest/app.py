0f19223c83b63622163fa47b14ebf0438ed63efb
pows <joe@ma.se>
2020-12-21 11:04:10 +0200

    removed redis counter, but added stylesheet

--- composetest/app.py
+++ composetest/app.py
@@ -1,6 +1,6 @@
 import time
 import redis
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
@@ -18,6 +18,6 @@ def get_hit_count():
 
 @app.route('/')
 def hello():
-    count = get_hit_count()
-    return f'<h1 style=\"color:red;background:green;\">Hello World! I have been seen {count} times.</h1>\n'
-
+    # count = get_hit_count()
+    # return f'<h1 style=\"color:red;background:green;\">Hello World! I have been seen {count} times.</h1>\n'
+    return render_template("main.html")
