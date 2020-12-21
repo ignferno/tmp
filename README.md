9d78d574af152bd9fe4145d37beb2a5207f5b6b3
pows <joe@ma.se>
2020-12-21 10:26:29 +0200

    playing around with compose and flask

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # TMP
 refers to temporary or template repositories. Often the readme file in each directory will contain info on how to generate, run or modify contain associated content
+
+Install docker
+- manage docker as non-root user
+- - docker gives various errors when not executed with sudo or as root
+https://docs.docker.com/engine/install/linux-postinstall/
+
+cmds:
+docker ps
+docker log
+
+# create react app
+Install npm
+npx create-react-app <name>
+
---
+++ composetest/app.py
@@ -0,0 +1,23 @@
+import time
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('kallits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return f'<h1 style=\"color:red;background:green;\">Hello World! I have been seen {count} times.</h1>\n'
+
